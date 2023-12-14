cd677f0861996556755778c17743f78e87916628
chryzcode <alabaolanrewaju13@gmail.com>
2023-12-14 11:17:42 +0100

    push

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require('./middleware/not-found')
 
 //middlewarwe
 app.use(express.static('./public'))
 app.use(express.json());
+app.use(notFound)
 
 //routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
